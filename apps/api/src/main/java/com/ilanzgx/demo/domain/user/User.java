97b2ffdfe5a66403bd5d0b591031d9e262f7a400
ilanzgx <ilan-_@hotmail.com>
2025-09-05 23:08:46 -0300

    infra: dockerize and containerized all services so far

---
+++ apps/api/src/main/java/com/ilanzgx/demo/domain/user/User.java
@@ -0,0 +1,24 @@
+package com.ilanzgx.demo.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+}
