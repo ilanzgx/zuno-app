bf9cfca21cce7f4e58f09e8632d04c5aefb6301e
ilanzgx <ilan-_@hotmail.com>
2025-09-07 11:57:31 -0300

    hotfix: ignoring id attribute in UserMapper.toEntity

--- apps/api/src/main/java/com/ilanzgx/demo/modules/user/application/UserMapper.java
+++ apps/api/src/main/java/com/ilanzgx/demo/modules/user/application/UserMapper.java
@@ -1,11 +1,14 @@
 package com.ilanzgx.demo.modules.user.application;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.ilanzgx.demo.modules.user.domain.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
     User toEntity(UserRequest userRequest);
+
     UserResponse toResponse(User user);
 }
