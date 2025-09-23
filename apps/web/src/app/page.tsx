6fba37b196bcd80397e43ccdb17fc86cf17eff6f
ilanzgx <ilan-_@hotmail.com>
2025-09-22 23:52:11 -0300

    feat(web): adding shadcn and first pages

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Hello World!</h1>
+      <div className="flex">
+        <div className="w-1/2">
+          <Link href="/entrar" passHref>
+            <Button>Entrar</Button>
+          </Link>
+        </div>
+        <div className="w-1/2">
+          <Button>Cadastrar</Button>
+        </div>
+      </div>
     </>
   );
 }
