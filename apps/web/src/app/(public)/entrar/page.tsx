14b0f5b77af1dfa3fcb58df739550f9146d2d932
ilanzgx <ilan-_@hotmail.com>
2025-09-23 22:48:24 -0300

    feat(web): componentes shadcn e formulario inicial de login

--- apps/web/src/app/(public)/entrar/page.tsx
+++ apps/web/src/app/(public)/entrar/page.tsx
@@ -1,7 +1,75 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z.email({ message: "Por favor, insira um email válido." }),
+  password: z.string().min(1, { message: "A senha é obrigatória." }),
+});
+
 export default function Entrar() {
+  const signInForm = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // TODO: Implementar a lógica de autenticação
+    console.log(values);
+  }
+
   return (
-    <>
+    <div>
       <h1>Entrar</h1>
-    </>
+      <Form {...signInForm}>
+        <form
+          onSubmit={signInForm.handleSubmit(onSubmit)}
+          className="grid gap-4"
+        >
+          <FormField
+            control={signInForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="seu@email.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signInForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Sua senha" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Entrar</Button>
+        </form>
+      </Form>
+    </div>
   );
 }
